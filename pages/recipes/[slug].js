0936a720f4ff9a6c70e91951a785bc14cd90cab5
knifecome <qhx717@qq.com>
2021-07-23 10:58:54 +0800

    update2

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -4,6 +4,7 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import Image from 'next/image'
 
 
+
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
